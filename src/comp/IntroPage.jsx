2d816f0fee6e57e4fbe052ea3ce9d1c06db01815
Landsmanadav <landsmanadav@gmail.com>
2022-05-24 22:44:08 +0300

    secondcommit

---
+++ src/comp/IntroPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+export default function IntroPage() {
+    return (
+        <div className="introPageContainer">
+
+        </div >
+    )
+}
+{/* <motion.h1
+animate={{ fontSize: "50px", color: "#ff2994" }}
+transition={{ duration: 3 }}
+> Intro Page </motion.h1> */}
